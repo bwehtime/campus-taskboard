30523a01529477aa291c6da7f333a2fe959e4f1c
Li Ying Feng <06.liy.feng@gmail.com>
2026-04-17 22:29:29 -0400

    Initial commit

---
+++ src/main/java/edu/brooklyn/cisc3130/campus_taskboard/CampusTaskboardApplication.java
@@ -0,0 +1,14 @@
+package edu.brooklyn.cisc3130.campus_taskboard;
+
+import org.apache.catalina.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusTaskboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusTaskboardApplication.class, args);
+	}
+
+}
